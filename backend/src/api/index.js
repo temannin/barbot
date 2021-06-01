4ed00e605300cbd1e13fd5d23aa5b815812f02f8
Tyler Manning <tylerericmanning@gmail.com>
2021-06-01 00:11:19 -0400

    Added queue for drinks being made

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const emojis = require("./emojis");
 const router = express.Router();
+const queue = require("express-queue");
 
 const { drinks } = require("../drinks_config");
 
@@ -8,15 +9,23 @@ router.get("/drinks", (req, res) => {
   res.json(drinks);
 });
 
-router.post("/tend", (req, res) => {
-  console.log(req.body);
-  res.json("Making...");
+router.post("/tend", queue({ activeLimit: 1, queuedLimit: -1 }), (req, res) => {
+  sleep(5000).then(() => {
+    res.json("Test");
+  });
 });
 
 router.get("/settings", (req, res) => {
   res.json([{}, {}, {}, {}, {}, {}, {}, {}]);
 });
 
+router.post("/settings", (req, res) => {
+})
+
 router.use("/emojis", emojis);
+
+function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
 
 module.exports = router;
